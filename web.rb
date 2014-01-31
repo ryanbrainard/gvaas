985a37ea7bd5e3b3dad8f3c700b21d4eb5b9beb6
Ryan Brainard <brainard@heroku.com>
2014-01-31 15:38:15 -0800

    Add schema response at root

--- web.rb
+++ web.rb
@@ -1,17 +1,33 @@
 require 'sinatra'
 require 'open3'
+require 'json'
+
+gv_type = 'text/vnd.graphviz'
+
+supported_transforms = {
+    '*/*'           => 'dot',
+    gv_type         => 'dot',
+    #'image/png'    => 'dot -Tpng',
+    #'image/gif'    => 'dot -Tgif',
+    'image/svg+xml' => 'dot -Tsvg'
+}
+
+get '/' do
+  content_type :json
+  JSON.pretty_generate ({
+    name: 'GVaaS: Graphviz as a Service',
+    repository: 'https://github.com/ryanbrainard/gvaas',
+    resources: {
+        url: '/dot',
+        method: 'POST',
+        content_type: 'text/vnd.graphviz',
+        accept: supported_transforms.keys
+    }
+  })
+end
 
 post '/dot' do
-  gv_type = 'text/vnd.graphviz'
   error(415) unless request.media_type == gv_type
-
-  supported_transforms = {
-      '*/*'           => 'dot',
-       gv_type        => 'dot',
-      'image/png'     => 'dot -Tpng',
-      'image/gif'     => 'dot -Tgif',
-      'image/svg+xml' => 'dot -Tsvg'
-  }
 
   accept = request.accept ? request.accept.first : gv_type
   transform = supported_transforms.detect{|t,_| t == accept}
